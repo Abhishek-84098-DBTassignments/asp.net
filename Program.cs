510eae79aa2c59ca5a2a5605234816e6d1a67926
joshiabhishek0392 <162970629+joshiabhishek0392@users.noreply.github.com>
2024-07-04 16:16:31 +0530

    Students Array submitted

--- Program.cs
+++ Program.cs
@@ -1,85 +1,86 @@
-﻿using System;
-using System.Security.Cryptography.X509Certificates;
+﻿namespace StudentArray;
 
-namespace StudentStructure
+    public struct Student
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Student student = new Student("Abhi", true, 18, 12, 'D', 85.5);
-            student.ToString();
-            student.AcceptDetails();
-            student.PrintDetails();
+    public string Name;
+    public bool Gender;
+    public int Age;
+    public int Std;
+    public char Div;
+    public double Marks;
+}
 
-        }
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number of students: ");
+        int size = Convert.ToInt32(Console.ReadLine());
+
+        Student[] students = CreateArray(5);
+        AcceptInfo(students);
+        PrintInfo(students);
+        Student[] reversedStudents = ReverseArray(students);
+        Console.WriteLine("\nReversed Student Information:");
+        PrintInfo(reversedStudents);
+
+
+
     }
-    public class Student
+    public static Student[] CreateArray(int size)
     {
-        private string _Name;
-        private bool _Gender;
-        private int _Age;
-        private int _Std;
-        private char _Div;
-        private double _Marks;
-
-        public Student()
+        Student[] students = new Student[size];
+        return students;
+    }
+    public static void AcceptInfo(Student[] students)
+    {
+        for (int i = 0; i < students.Length; i++)
         {
-            
-        }
-
-        public Student(string name, bool gender, int age, int std, char div, double marks)
-        {
-            Name = name;
-            Gender = gender;
-            Age = age;
-            Std = std;
-            Div = div;
-            Marks = marks;
-        }
-
-        public string Name { get => _Name; set => _Name = value; }
-        public bool Gender { get => _Gender; set => _Gender = value; }
-        public int Age { get => _Age; set => _Age = value; }
-        public int Std { get => _Std; set => _Std = value; }
-        public char Div { get => _Div; set => _Div = value; }
-        public double Marks { get => _Marks; set => _Marks = value; }
-
-        public override string? ToString()
-        {
-            return base.ToString();
-        }
-        public void AcceptDetails()
-        {
-            Console.WriteLine("Enter student details:");
+            Console.WriteLine($"\nEnter details for student {i + 1}:");
             Console.Write("Name: ");
-            Name = Console.ReadLine();
+            students[i].Name = Console.ReadLine();
 
             Console.Write("Gender (true for male, false for female): ");
-            Gender = Convert.ToBoolean(Console.ReadLine());
+            students[i].Gender = Convert.ToBoolean(Console.ReadLine());
 
             Console.Write("Age: ");
-            Age = Convert.ToInt32(Console.ReadLine());
+            students[i].Age = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Standard: ");
-            Std = Convert.ToInt32(Console.ReadLine());
+            students[i].Std = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Division: ");
-            Div = Convert.ToChar(Console.ReadLine());
+            students[i].Div = Convert.ToChar(Console.ReadLine());
 
             Console.Write("Marks: ");
-            Marks = Convert.ToDouble(Console.ReadLine());
+            students[i].Marks = Convert.ToDouble(Console.ReadLine());
         }
-        public void PrintDetails()
-        {
-            Console.WriteLine("\nStudent Details:");
-            Console.WriteLine($"Name: {Name}");
-            Console.WriteLine($"Gender: {(Gender ? "Male" : "Female")}");
-            Console.WriteLine($"Age: {Age}");
-            Console.WriteLine($"Standard: {Std}");
-            Console.WriteLine($"Division: {Div}");
-            Console.WriteLine($"Marks: {Marks}");
-        }
-
     }
-}
+    public static void PrintInfo(Student[] students)
+    {
+        Console.WriteLine("\nStudent Information:");
+        for (int i = 0; i < students.Length; i++)
+        {
+            Console.WriteLine($"\nDetails of student {i + 1}:");
+            Console.WriteLine($"Name: {students[i].Name}");
+            Console.WriteLine($"Gender: {(students[i].Gender ? "Male" : "Female")}");
+            Console.WriteLine($"Age: {students[i].Age}");
+            Console.WriteLine($"Standard: {students[i].Std}");
+            Console.WriteLine($"Division: {students[i].Div}");
+            Console.WriteLine($"Marks: {students[i].Marks}");
+        }
+    }
+    public static Student[] ReverseArray(Student[] students)
+    {
+        Student[] reversedArray = new Student[students.Length];
+
+        for (int i = 0, j = students.Length - 1; i < students.Length; i++, j--)
+        {
+            reversedArray[i] = students[j];
+        }
+        return reversedArray;
+    }
+  }
+
